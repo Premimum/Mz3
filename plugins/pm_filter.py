6131ed91de3977bde0ea6d7e3b9208c09af774fe
Premimum <129716070+Premimum@users.noreply.github.com>
2023-06-07 20:06:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1439,7 +1439,7 @@ async def auto_filter(client, msg, spoll=False):
             )
 
     btn.insert(0, [
-        InlineKeyboardButton("🎦  𝐇𝐎𝐖 𝐓𝐎 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃  🎦", url="t.me/MM_DVD_UPDATES")
+        InlineKeyboardButton("⚡️ Cʜᴇᴄᴋ Bᴏᴛ PM ⚡️", url="https://t.me/Auto_Filter_MZBot")
     ])
 
     if offset != "":
