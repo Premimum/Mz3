183695fc7ab1d6582ca3214318edae78fd3c54f9
AKR <98525798+Akrtechi@users.noreply.github.com>
2023-07-21 18:58:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -161,7 +161,7 @@ async def start(client, message):
                           InlineKeyboardButton('Gʀᴏᴜᴘ', url=GRP_LNK),
                           InlineKeyboardButton('Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                        ],[
-                          InlineKeyboardButton("Bᴏᴛ Cʀᴇᴀᴛᴏʀ", url="t.me/Botx_Support")
+                          InlineKeyboardButton("Bᴏᴛ Cʀᴇᴀᴛᴏʀ", url="t.me/+rOTHzM8jL_c2MzBl")
                          ]
                         ]
                     )
@@ -180,7 +180,7 @@ async def start(client, message):
                           InlineKeyboardButton('Gʀᴏᴜᴘ', url=GRP_LNK),
                           InlineKeyboardButton('Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                        ],[
-                          InlineKeyboardButton("Bᴏᴛ Cʀᴇᴀᴛᴏʀ", url="t.me/Botx_Support")
+                          InlineKeyboardButton("Bᴏᴛ Cʀᴇᴀᴛᴏʀ", url="t.me/+rOTHzM8jL_c2MzBl")
                          ]
                         ]
                     )
@@ -281,7 +281,7 @@ async def start(client, message):
                       InlineKeyboardButton('Gʀᴏᴜᴘ', url=GRP_LNK),
                       InlineKeyboardButton('Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                    ],[
-                      InlineKeyboardButton("Bᴏᴛ Cʀᴇᴀᴛᴏʀ", url="t.me/Botx_Support")
+                      InlineKeyboardButton("Bᴏᴛ Cʀᴇᴀᴛᴏʀ", url="t.me/+rOTHzM8jL_c2MzBl")
                      ]
                     ]
                 )
@@ -334,7 +334,7 @@ async def start(client, message):
               InlineKeyboardButton('Gʀᴏᴜᴘ', url=GRP_LNK),
               InlineKeyboardButton('Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
            ],[
-              InlineKeyboardButton("Bᴏᴛ Cʀᴇᴀᴛᴏʀ", url="t.me/Botx_Support")
+              InlineKeyboardButton("Bᴏᴛ Cʀᴇᴀᴛᴏʀ", url="t.me/+rOTHzM8jL_c2MzBl")
              ]
             ]
         )
